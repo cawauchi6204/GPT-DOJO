cc91cd23495214b6f8030e8d1ce9dcb4b8d77ca5
cawauchi6204 <cawauchi6204@gmail.com>
2025-02-03 21:36:12 +0900

    ✨ UI修正

--- src/app/study/StudyClient.tsx
+++ src/app/study/StudyClient.tsx
@@ -175,7 +175,7 @@ export default function StudyClient({
     <Layout hideFooter={true}>
       <div className="flex flex-col md:flex-row h-[calc(100vh-64px)] relative">
         {/* 左側:説明エリア */}
-        <div className="h-[40%] md:h-auto w-full md:w-[30%] lg:w-[20%] bg-gray-50 flex flex-col overflow-y-auto">
+        <div className="h-1/2 md:h-auto w-full md:w-[30%] bg-gray-50 flex flex-col overflow-y-auto">
           <div className="flex-1 p-4 md:p-6">
             <div className="max-w-2xl mx-auto">
               <h1 className="text-xl md:text-2xl font-bold mb-4">
@@ -307,7 +307,7 @@ export default function StudyClient({
         </div>
 
         {/* 右側:ChatGPTスタイルのインターフェース */}
-        <div className="h-[60%] md:h-auto w-full md:w-[70%] lg:w-[80%] flex-1 bg-[#1a1a1a] flex flex-col overflow-y-scroll [scrollbar-width:none] [-ms-overflow-style:none] [&::-webkit-scrollbar]:hidden">
+        <div className="h-1/2 md:h-auto w-full md:w-[70%] flex-1 bg-[#1a1a1a] flex flex-col overflow-y-scroll [scrollbar-width:none] [-ms-overflow-style:none] [&::-webkit-scrollbar]:hidden">
           {/* メッセージ表示エリア */}
           <div className="flex-1 overflow-y-auto p-3 md:p-4">
             <div className="max-w-3xl mx-auto space-y-4">
